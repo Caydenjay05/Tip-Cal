f0731c26ec476f73dce581c7c7e79b0a98575572
Cayden Cannata <caydenjay05@gmail.com>
2026-06-23 15:05:11 -0400

    adding bill spliter

--- tip.js
+++ tip.js
@@ -9,7 +9,17 @@ document.getElementById("calculateButton").onclick = function () // Calculate ti
     let splitCount = document.getElementById("splitCount").value; // Get number of people to split the bill
     let totalPerPerson = (parseFloat(bill) + tip) / splitCount; // Calculate total amount per person
     
+    
 
     document.getElementById("result").textContent =
         "Tip Amount: $" + tip.toFixed(2) + "\nTotal Per Person: $" + totalPerPerson.toFixed(2);// Display tip amount and total per person in result div
 };
+document.getElementById("billSplit").oninput = function () // Calculate tip amount
+{
+    let bill = document.getElementById("billAmount").value;// Get bill amount from input field
+    let amountofPeople = document.getElementById("billSplit").value; // Get number of people to split the bill
+    let totalPerPerson = bill / amountofPeople; // Calculate total amount per person
+
+    document.getElementById("resultbill").textContent =
+        "Total Per Person: $" + totalPerPerson.toFixed(2);// Display total per person in result div
+}
