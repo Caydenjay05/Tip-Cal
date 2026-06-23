02a4d4ca4252b752b39b3736a26c1efde26a03cd
Cayden Cannata <caydenjay05@gmail.com>
2026-06-23 13:52:06 -0400

    adding split to amount

--- tip.js
+++ tip.js
@@ -8,7 +8,8 @@ document.getElementById("calculateButton").onclick = function () // Calculate ti
     let tip = bill * (tipPercent / 100);// Calculate tip amount
     let splitCount = document.getElementById("splitCount").value; // Get number of people to split the bill
     let totalPerPerson = (parseFloat(bill) + tip) / splitCount; // Calculate total amount per person
+    
 
     document.getElementById("result").textContent =
-        "Tip Amount: $" + tip.toFixed(2);// Display tip amount in result div
+        "Tip Amount: $" + tip.toFixed(2) + "\nTotal Per Person: $" + totalPerPerson.toFixed(2);// Display tip amount and total per person in result div
 };
